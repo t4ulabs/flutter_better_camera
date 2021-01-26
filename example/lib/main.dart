968f1db10c491d29f13bdf167fb00f7a9c98eade
habibhentati <habib.hentati@esprit.tn>
2021-01-26 12:30:58 +0100

    plugin integration

---
+++ example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter_better_camera/camera.dart';
+import 'package:flutter/material.dart';
+import './camera_home.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    //logError(e.code, e.description);
+  }
+  runApp(CameraApp());
+}
+
+class CameraApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraHome(
+        cameras: cameras,
+      ),
+    );
+  }
+}
